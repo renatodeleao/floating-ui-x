1c7e673822e85763a72c0eec1c685c54d65bec5c
Renato de Leão <renatodeleao@gmail.com>
2022-10-25 18:14:13 +0100

    feat(vue-dom): add vue-ifyied arrow middleware

--- packages/vue-dom/src/arrow.js
+++ packages/vue-dom/src/arrow.js
@@ -1,1 +1,19 @@
-export function arrow() {}
+import { unref } from 'vue';
+import { arrow as arrowCore } from '@floating-ui/dom';
+
+export function arrow(options) {
+  const { element, padding } = options;
+  return {
+    name: 'arrow',
+    options,
+    fn(args) {
+      const $element = unref(element);
+      if ($element != null) {
+        return arrowCore({ element: $element, padding }).fn(args);
+      }
+
+      return {};
+    },
+  };
+}
+
