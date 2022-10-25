3aa7afa8218c022a77479bf1fce3052a180ade07
Renato de Leão <renatodeleao@gmail.com>
2022-10-25 18:08:18 +0100

    feat(vue-dom): add useFloating initial implementation
    based on the on i've submitted to the original fork, in vanilla js

--- packages/vue-dom/src/useFloating.js
+++ packages/vue-dom/src/useFloating.js
@@ -1,1 +1,72 @@
-export function useFloating() {}
+import { ref, watch, toRefs, unref, reactive } from 'vue';
+import { computePosition } from '@floating-ui/dom';
+
+export function useFloating({
+  middleware = [],
+  placement = 'bottom',
+  strategy = 'absolute',
+  whileElementsMounted,
+} = {}) {
+  const reference = ref(null);
+  const floating = ref(null);
+  const middlewareRef = ref(middleware);
+  const cleanupRef = ref(null);
+  const placementRef = ref(placement);
+  const strategyRef = ref(strategy);
+
+  const data = reactive({
+    // Setting these to `null` will allow the consumer to determine if
+    // `computePosition()` has run yet
+    x: null,
+    y: null,
+    placement: unref(placement),
+    strategy: unref(strategy),
+    middlewareData: ref({}),
+  });
+
+  const update = () => {
+    if (!reference.value || !floating.value) {
+      return;
+    }
+
+    return computePosition(reference.value, floating.value, {
+      middleware: unref(middlewareRef),
+      placement: unref(placement),
+      strategy: unref(strategy),
+    }).then((computedData) => {
+      Object.assign(data, computedData);
+      return computedData;
+    });
+  };
+
+  watch([placementRef, strategyRef], update);
+  watch(middlewareRef, update, { deep: true });
+
+  watch([reference, floating], () => {
+    if (cleanupRef.value) {
+      cleanupRef.value();
+      cleanupRef.value = null;
+    }
+
+    if (!reference.value || !floating.value) {
+      return;
+    }
+
+    if (whileElementsMounted) {
+      cleanupRef.value = whileElementsMounted(
+        reference.value,
+        floating.value,
+        update
+      );
+    } else {
+      update();
+    }
+  });
+
+  return {
+    ...toRefs(data),
+    update,
+    reference,
+    floating,
+  };
+}
